17d23ab9cab55f85c39ea220f65250e7c458b647
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-02-24 21:53:12 +0300

    Update Location.java

--- Location.java
+++ Location.java
@@ -2,5 +2,6 @@ public class Location {
     private Long x; //Поле не может быть null
     // лишняя строка
     private Double y; //Поле не может быть null
+    // Напиши мне. Просто интересно, как скоро ты это заметишь 
     private String name; //Поле не может быть null
 }
