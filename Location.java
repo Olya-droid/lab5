9bdaa520dcde249604b7c68b71d11bb0e8ed62b7
Olya-droid <61423764+Olya-droid@users.noreply.github.com>
2020-02-24 20:46:56 +0300

    Add files via upload

---
+++ Location.java
@@ -0,0 +1,5 @@
+public class Location {
+    private Long x; //Поле не может быть null
+    private Double y; //Поле не может быть null
+    private String name; //Поле не может быть null
+}
---
+++ Route.java
@@ -0,0 +1,9 @@
+public class Route {
+        private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
+        private String name; //Поле не может быть null, Строка не может быть пустой
+        private Coordinates coordinates; //Поле не может быть null
+        private java.time.LocalDate creationDate; //Поле не может быть null, Значение этого поля должно генерироваться автоматически
+        private Location from; //Поле может быть null
+        private Location to; //Поле не может быть null
+        private Float distance; //Поле может быть null, Значение поля должно быть больше 1
+}
