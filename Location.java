5e4b6e3b4b829b9ebe3757e2b16e84adccc265df
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-04-12 02:07:48 +0300

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,7 +1,31 @@
 public class Location {
     private Long x; //Поле не может быть null
-    // лишняя строка
     private Double y; //Поле не может быть null
-    // Напиши мне. Просто интересно, как скоро ты это заметишь 
     private String name; //Поле не может быть null
+
+    public Location(Long x, Double y,  String name) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+    }
+    public Long getX() {
+        return x;
+    }
+    public Double getY() {
+        return y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Location(" +
+                "x = " + x +
+                ", y = " + y +
+                ", name = " + name +
+                ')';
+    }
+
 }
