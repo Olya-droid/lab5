9ec50d9160f33987344fa0e0fd1a9e7f7a0955d0
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-04-12 02:06:24 +0300

    Update Coordinates.java

--- Coordinates.java
+++ Coordinates.java
@@ -1,5 +1,24 @@
 public class Coordinates {
+    public Coordinates(int x,  float y) {
+        this.x = x;
+        this.y = y;
+    }
     private int x; //Значение поля должно быть больше -836
-    //something
-    private Float y; //Максимальное значение поля: 840, Поле не может быть null
+    private float y; //Максимальное значение поля: 840, Поле не может быть null
+
+    public int getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinates(" +
+                "x = " + x
+                +  ", y = " + y
+                + ')';
+    }
 }
