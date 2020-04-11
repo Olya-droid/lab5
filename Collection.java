d5d69942e594e679bcf13f5863f739dcbc5a4a64
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-04-12 02:05:52 +0300

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -1,13 +1,23 @@
 import java.util.*;
+import java.io.File;
 
 public class Collection {
 
     public static ArrayList <Route> routes = new ArrayList<>();
-
 
     public static void show (){
         for (Route n: routes){
             System.out.println("  " + n.toString());
         }
     }
+
+
+    public Route searchById(Integer id) {
+        for (Route r : routes) {
+            if (r.getId().equals(id))
+                return r;
+        }
+        return null;
+    }
+
 }
