09b8f3eb53d80f17f02cd9c6c15439ac210739ad
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-13 02:20:46 +0500

    Update IDGenerator.java

--- src/Generators/IDGenerator.java
+++ src/Generators/IDGenerator.java
@@ -1,16 +1,9 @@
 package Generators;
 
-import java.util.ArrayList;
-import Routes.*;
-
 public class IDGenerator {
 
-    public static long generateNewID(ArrayList<Route> Routes){
-        long id;
-        do {
-            id = (long) (1 + Math.random() * (Long.MAX_VALUE - 1));
-        } while (Collection.searchById(id) != null);
-        return id;
+    public static long generateNewID(){
+        return (long) (1 + Math.random() * (Long.MAX_VALUE - 1));
     }
 
 }
