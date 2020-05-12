7fd57eddfc998d6855089fdac66f0ec696faedca
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-12 20:47:24 +0500

    Update ConsoleSourceReader.java

--- src/Readers/ConsoleSourceReader.java
+++ src/Readers/ConsoleSourceReader.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 public class ConsoleSourceReader extends Reader{
 
     public ConsoleSourceReader(){
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     }
 
 }
