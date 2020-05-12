281f243ee2ea7e5b783d40e663754b29b3f69f24
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-13 01:31:08 +0500

    Update Reader.java

--- src/Readers/Reader.java
+++ src/Readers/Reader.java
@@ -1,12 +1,14 @@
 package Readers;
 
+import Exceptions.EndOfSourceException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public abstract class Reader {
 
     protected BufferedReader bufferedReader;
 
-
-
-
+    public abstract String getLine() throws EndOfSourceException, IOException;
+    
 }
