097d75715ade5bf05c49bce8353934b0e45652b3
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-12 20:04:47 +0500

    Update Reader.java

--- src/Readers/Reader.java
+++ src/Readers/Reader.java
@@ -1,4 +1,23 @@
 package Readers;
 
-public abstract class Reader {
+import Exceptions.WrongFileNameException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
+
+public class FileSourceReader extends Reader {
+
+    private String path;
+
+    public FileSourceReader(String path) throws WrongFileNameException {
+        this.path = path;
+        try {
+            this.bufferedReader = new BufferedReader(new FileReader(path));
+        } catch (FileNotFoundException e) {
+            throw new WrongFileNameException("Файл с данным именем не найден!");
+        }
+    }
+
+    
 }
