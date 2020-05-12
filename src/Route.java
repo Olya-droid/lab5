366c0cb8b6dbbfc93d5edbd6604cade2dee4a9c7
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-12 19:05:26 +0300

    Update Route.java

--- src/Route.java
+++ src/Route.java
@@ -1,6 +1,5 @@
-
 public class Route {
-    //public static long lastID = 0; // id прошлого элемента, увеличивается при появлении нового элемента
+    private static long lastID = 0; // id прошлого элемента, увеличивается при появлении нового элемента
     private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -10,8 +9,8 @@ public class Route {
     private Float distance; //Поле может быть null, Значение поля должно быть больше 1
 
     public Route (String name, Coordinates coordinates, Location from, Location to, Float distance){
-        //Route.lastID +=1;
-        //this.id = lastID;
+        Route.lastID +=1;
+        this.id = lastID;
         this.name = name;
         this.coordinates = coordinates;
         this.creationDate = java.time.LocalDate.now();
@@ -34,9 +33,9 @@ public class Route {
                 ')';
     }
 
-   public Long getId() {
+    public Long getId() {
         return id;
-
+    }
 
     public void setId(Long id) {
         this.id = id;
