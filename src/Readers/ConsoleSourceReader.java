1e73b8814ff5e3353da9d1713e3bb28f76ee676f
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-13 01:32:20 +0500

    Update ConsoleSourceReader.java

--- src/Readers/ConsoleSourceReader.java
+++ src/Readers/ConsoleSourceReader.java
@@ -1,12 +1,24 @@
 package Readers;
 
+import Exceptions.EndOfSourceException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleSourceReader extends Reader{
 
     public ConsoleSourceReader(){
         this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    @Override
+    public String getLine() throws EndOfSourceException {
+        try {
+            return bufferedReader.readLine();
+        } catch(IOException e){
+            throw new EndOfSourceException();
+        }
     }
 
 }
