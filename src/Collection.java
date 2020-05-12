f107b26502d38d88e4d45d88255d319b6adb53dc
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-12 22:22:33 +0500

    Update Collection.java

--- src/Collection.java
+++ src/Collection.java
@@ -1,18 +1,28 @@
-import java.util.*;
+import Generators.DateGenerator;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
 import java.io.File;
 
 public class Collection {
 
-    public static ArrayList <Route> Routes = new ArrayList<>();
+    private static ArrayList<Route> Routes;
 
-    public static void show (){
+    private LocalDate initializationDate;
+
+    public Collection(){
+        ArrayList<Route> Routes = new ArrayList<>();
+        initializationDate = DateGenerator.generateCurrentDate();
+    }
+
+    public static void show(){
         for (Route n: Routes){
             System.out.println("  " + n.toString());
         }
     }
 
 
-    public Route searchById(Integer id) {
+    public Route searchById(Long id) {
         for (Route r : Routes) {
             if (r.getId().equals(id))
                 return r;
