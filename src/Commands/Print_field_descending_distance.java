d55de9ab10fdf2af3dccee7a38983b711d518bb4
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 20:00:37 +0300

    Update Print_field_descending_distance.java

--- src/Commands/Print_field_descending_distance.java
+++ src/Commands/Print_field_descending_distance.java
@@ -1,8 +1,23 @@
 package Commands;
 
+import Routes.Collection;
+import Routes.Route;
+
+import java.util.Arrays;
+
 public class Print_field_descending_distance {
         static String description ="print_field_descending_distance: вывести значение поля distance в порядке убывания.";
-public static void print_field_descending_distance () {
+public static void print_field_descending_distance (Collection c) {
+        float [] array = new float[c.Routes.size() - 1 ];
+        int i = 0;
+        for (Route r: c.Routes) {
+                array[i] = r.getDistance();
+                i ++;
+        }
 
+        Arrays.sort(array);
+        for (i =c.Routes.size() - 1; i>=0; i-- ){
+                System.out.println(array[i]);
+        }
 }
 }
