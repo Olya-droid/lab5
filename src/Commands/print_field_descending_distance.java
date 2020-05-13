4c321baf01a8223f5c34ca0f319b0db6d799e661
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 15:26:08 +0300

    Delete print_field_descending_distance.java

--- src/Commands/print_field_descending_distance.java
+++
@@ -1,6 +0,0 @@
-package Commands;
-
-public class print_field_descending_distance {
-        static String description ="print_field_descending_distance: вывести значение поля distance в порядке убывания.";
-
-}
