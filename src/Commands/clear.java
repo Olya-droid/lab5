243ab52561d44dbf49429a2627a7051f1f502274
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 15:25:47 +0300

    Delete clear.java

--- src/Commands/clear.java
+++
@@ -1,8 +0,0 @@
-package Commands;
-import java.util.ArrayList;
-public class clear {
-    static String description ="clear : очистить коллекцию";
-    public static void clear(ArrayList c) {
-        c.clear();
-    }
-}
