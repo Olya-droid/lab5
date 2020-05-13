b68b79dac2ab3b04df956b8c97853d3b70e9132a
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-14 00:41:07 +0500

    Update Collection.java

--- src/Routes/Collection.java
+++ src/Routes/Collection.java
@@ -3,9 +3,16 @@ import Generators.IDGenerator;
 import Generators.DateGenerator;
 import java.util.ArrayList;
 import java.time.LocalDate;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Collection {
 
+    @XmlElement
     public  ArrayList<Route> Routes;
     private LocalDate initializationDate;
 
