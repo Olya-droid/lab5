29799b31ef521ce5b3d8f9875ed23d5450df4053
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 15:25:36 +0300

    Delete execute_script.java

--- src/Commands/execute_script.java
+++
@@ -1,6 +0,0 @@
-package Commands;
-
-public class execute_script {
-    static String description ="execute_script file_name : считать и исполнить скрипт из указанного файла.В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
-
-}
