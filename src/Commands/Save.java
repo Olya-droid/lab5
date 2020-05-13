abd473b072e2ba3ccd084f234f2e197ba950271b
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-14 04:38:26 +0500

    есть шанс что это может сработать

--- src/Commands/Save.java
+++ src/Commands/Save.java
@@ -1,8 +1,18 @@
 package Commands;
 
-public class Save {
-        static String description ="save : сохранить коллекцию в файл";
-        public static void save (Routes.Collection c){
+import Exceptions.FileFormatException;
+import Exceptions.WrongFileNameException;
 
-        }
+import static XmlManagers.Saver.saveCollection;
+
+public class Save {
+
+    static String description ="save : сохранить коллекцию в файл";
+
+    public static void save (Routes.Collection c) throws WrongFileNameException, FileFormatException {
+
+        saveCollection(c, "input.xml");
+
+    }
+
 }
