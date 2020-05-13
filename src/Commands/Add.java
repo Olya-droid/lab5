043f1d5a70f76e14a5f7553163c71062563b819a
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 18:14:21 +0300

    Update Add.java

--- src/Commands/Add.java
+++ src/Commands/Add.java
@@ -1,7 +1,6 @@
 package Commands;
-
-import Routes.Collection;
 import Readers.*;
+import Routes.*;
 
 import java.time.ZonedDateTime;
 
@@ -9,7 +8,33 @@ public class Add {
     static String description = "add {element} : добавить новый элемент в коллекцию";
 
     public static void add(Reader reader, Collection c, String s2) {
-        c.Routes.add(); /// доделать
+                System.out.println("ну вот хочется тебе своими файлами место занимать?");
+        Route route = new Route();
+        route.setId(c.generateUniqueID());
+        String name =
+        route.setName(name);
+                System.out.println("Можно было конечно и поинтереснее имя придумать. В следующий раз постарайся получше");
+                System.out.println("Ну давай, будем надеяться координаты поинтереснее будут");
+        int coordinateX =
+        float coordinateY =
+        route.setCoordinates(coordinateX, coordinateY);
+                System.out.println("Нет, ну это уже никуда не годится, кому такие координаты нужны? Ладно, может хоть локейшн нормальный будет");
+                System.out.println("Откуда? (from)");
+        long locationFromX =
+        double locationFromY =
+        String locationFromName =
+        route.setFrom(new Location(locationFromX, locationFromY, locationFromName));
+                System.out.println("Куда? (to)");
+        long locationToX =
+        double locationToY =
+        String locationToName =
+        route.setTo(new Location(locationToX, locationToY, locationToName));
+                System.out.println("ну вот, хоть координаты нормальные. Умеешь ведь, когда захочешь");
+                System.out.println("Давай посмотрим, что у нас там с Distance");
+        float distance =
+        route.setDistance(distance);
+        c.Routes.add(route);
+                System.out.println("Ну что, добился чего хотел?");
     }
 
 
