45d7c5dbc756cf41fc8371147f36ae75d24c0753
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-13 20:05:06 +0500

    Update CommandArgumentSplitter.java

--- src/Readers/CommandArgumentSplitter.java
+++ src/Readers/CommandArgumentSplitter.java
@@ -8,8 +8,15 @@ public class CommandArgumentSplitter {
     public static String[] comArgSplitter(String readLine) throws NoCommandException {
 
         if (readLine != null) {
+            
+            readLine = readLine.trim();
             String [] ComArg = readLine.split(" ", 2);
+
+            if (ComArg.length == 2) {
+                ComArg[1] = ComArg[1].trim();
+            }
             return ComArg;
+
         }
 
         throw new NoCommandException();
