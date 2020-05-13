7b5400ddc14e58a5f5ed769d9f6693bef4947947
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 15:24:00 +0300

    Delete average_of_distance.java

--- src/Commands/average_of_distance.java
+++
@@ -1,7 +0,0 @@
-package Commands;
-
-public class average_of_distance {
-    static String description ="average_of_distance : вывести среднее значение поля distance для всех элементов коллекции.";
-
-}
-
