f13f8a4971a5c20764d40aa2ba8e1219d13b1e5d
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-13 12:47:24 +0300

    Update info.java

--- src/Commands/info.java
+++ src/Commands/info.java
@@ -1,11 +1,10 @@
 package Commands;
 
-import java.util.Collection;
-
 public class info {
     static String description ="info : вывести в стандартный поток вывода информацию о коллекции (тип, дата инициализации, количество элементов и т.д.)";
-    public static void info(Collection c) {
-
+    public static void info(Routes.Collection c) {
+        System.out.println("Тип коллекции: " + c.Routes.getClass().getName());
+        System.out.println("Колличество элементов: " + c.Routes.size());
+        System.out.println("Коллеция создана: " + c.getInitializationDate());
     }
-
 }
