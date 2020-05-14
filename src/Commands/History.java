ea2842cd3fb9cd6c307c7fdb3dccf6efda4dbefe
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-14 13:25:53 +0300

    Update History.java

--- src/Commands/History.java
+++ src/Commands/History.java
@@ -2,7 +2,15 @@ package Commands;
 
 public class History {
     static String description ="history: вывести последние 13 команд (без их аргументов)";
+    static final int n = 12;
+    static String [] historyArray = new String[n];
 public static void history (){
-
+    for (int i = n-1; i>=0; i--) { if (historyArray[i] != null) System.out.println("   "+ (i+1) + "   " + historyArray[i]);}
 }
+public static void addInArray (String s1){
+        for (int i = n-1; i>0; i--) {
+            historyArray[i]=historyArray[i-1];
+        }
+        historyArray[0] = s1;
+    }
 }
