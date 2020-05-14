12bec36c254b5cbe56cb14210e4046990a8112fb
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-14 03:35:46 +0300

    Update Remove_by_id.java

--- src/Commands/Remove_by_id.java
+++ src/Commands/Remove_by_id.java
@@ -1,11 +1,14 @@
 package Commands;
 import Readers.*;
+import Routes.Collection;
 
 public class Remove_by_id {
 
         static String description ="remove_by_id id : удалить элемент из коллекции по его id";
-public static void remove_by_id(Reader reader, Routes.Collection c, String s2){
+public static void remove_by_id( Routes.Collection c, String s2){
+       long removeId = Checker.longChecker(s2);
 
+       c.Routes.remove(c.Routes.indexOf(c.searchById(removeId)));
 }
 
 }
