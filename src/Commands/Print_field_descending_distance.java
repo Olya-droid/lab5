827fbf892f53294dc02623979dbd8b00d594b751
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-14 13:31:45 +0300

    Update Print_field_descending_distance.java

--- src/Commands/Print_field_descending_distance.java
+++ src/Commands/Print_field_descending_distance.java
@@ -8,16 +8,18 @@ import java.util.Arrays;
 public class Print_field_descending_distance {
         static String description ="print_field_descending_distance: вывести значение поля distance в порядке убывания.";
 public static void print_field_descending_distance (Collection c) {
-        float [] array = new float[c.Routes.size()  ];
-        int i = 0;
-        for (Route r: c.Routes) {
-                array[i] = r.getDistance();
-                i ++;
-        }
+        if (!c.Routes.isEmpty()) {
+                float[] array = new float[c.Routes.size()];
+                int i = 0;
+                for (Route r : c.Routes) {
+                        array[i] = r.getDistance();
+                        i++;
+                }
 
-        Arrays.sort(array);
-        for (i =c.Routes.size() - 1; i>=0; i-- ){
-                System.out.println(array[i]);
-        }
+                Arrays.sort(array);
+                for (i = c.Routes.size() - 1; i >= 0; i--) {
+                        System.out.println(array[i]);
+                }
+        }else System.out.println("Коллекция пуста, в отличие от моего рабочего стола.");
 }
 }
