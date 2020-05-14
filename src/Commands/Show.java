0d3a243e30f6a3b099014700e44bd76192493c39
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-14 12:34:39 +0300

    Update Show.java

--- src/Commands/Show.java
+++ src/Commands/Show.java
@@ -4,10 +4,14 @@ import Routes.Route;
 
 public class Show {
     static String description ="show : вывести в стандартный поток вывода все элементы коллекции в строковом представлении";
-    public static void show (Collection c){
-        for (Route r: c.Routes){
-            System.out.println("  " + r.toString());
+    public static void show (Collection c) {
+        if (c.Routes.size() == 0) {
+            System.out.println("Коллекция пуста, милорд");
+        } else {
+
+            for (Route r : c.Routes) {
+                System.out.println("  " + r.toString());
+            }
         }
     }
-
 }
