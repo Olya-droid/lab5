73783e344095fa3d73ad04b869371abf5991fc03
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:26:03 +0300

    описание

--- src/Generators/DateGenerator.java
+++ src/Generators/DateGenerator.java
@@ -2,8 +2,15 @@ package Generators;
 
 import java.time.LocalDate;
 
+/**
+ * Генератор даты
+ */
 public class DateGenerator {
 
+    /**
+     * Получить актуальную дату
+     * @return LocalDate
+     */
     public static LocalDate generateCurrentDate() {
         return LocalDate.now();
     }
