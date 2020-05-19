05a394db7b8858f065da6e42798196ac2888c3d4
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:03:34 +0300

    0E2C2F0E2E190E2F1A0E2F1E0E2E190E2E2B0E2E2A0E2E19

--- src/Commands/History.java
+++ src/Commands/History.java
@@ -1,16 +1,29 @@
 package Commands;
 
+/**
+ * Команда "ПАМЯТЬ"
+ */
+
 public class History {
     static String description ="history: вывести последние 13 команд (без их аргументов)";
-    static final int n = 12;
-    static String [] historyArray = new String[n];
-public static void history (){
+    static final int n = 12; //количество сохраняемых команд -1
+    static String [] historyArray = new String[n]; //массив для хранения последних n+1 команд
+
+    /**
+     * Метод для отображения истории
+     */
+    public static void history (){
     for (int i = n-1; i>=0; i--) { if (historyArray[i] != null) System.out.println("   "+ (i+1) + "   " + historyArray[i]);}
 }
-public static void addInArray (String s1){
+
+    /**
+     * Запись команды в анналы истории
+     * @param s1 команда
+     */
+    public static void addInArray (String s1){
         for (int i = n-1; i>0; i--) {
             historyArray[i]=historyArray[i-1];
         }
-        historyArray[0] = s1;
+        historyArray[0] = s1; //запись команды в изначальном регистре в массив
     }
 }
