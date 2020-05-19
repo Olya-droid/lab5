1aff9221b06e814c039218b67ce60ff4f0f627e3
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:06:17 +0300

    Описание

--- src/Commands/Save.java
+++ src/Commands/Save.java
@@ -4,10 +4,18 @@ import java.io.FileNotFoundException;
 
 import static XmlManagers.Saver.saveCollection;
 
+/**
+ * Команда "Сохрани мою речь"
+ */
 public class Save {
 
         static String description ="save : сохранить коллекцию в файл";
 
+        /**
+         * Метод для сохранения коллекции в файл
+         * @param c коллекция
+         * @throws FileNotFoundException
+         */
         public static void save (Routes.Collection c) throws FileNotFoundException {
 
                 saveCollection(c, "./src/data/input.xml");
