15baa518ca2fe31b5fb6633b99ca3093dcceaa52
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:01:19 +0300

    описание

--- src/Commands/Help.java
+++ src/Commands/Help.java
@@ -1,10 +1,21 @@
 package Commands;
+
+/**
+ * Команда "ПОМОЩЬ"
+ */
 public  class Help {
+    /**
+     * Текстовый файл с описанием всех команд
+     */
     static String text = Add.description + "\n" + Average_of_distance.description + "\n" +
             Clear.description  + "\n" + Execute_script.description + "\n" + Exit.description + "\n" +
             History.description + "\n" + Info.description + "\n" + Max_by_distance.description + "\n" +
             Print_field_descending_distance.description + "\n" + Remove_by_id.description + "\n" + Reorder.description + "\n" +
             Save.description + "\n" + Show.description + "\n" + Sort.description + "\n" + Update.description;
+
+    /**
+     * Вывод описания всех команд
+     */
     public static  void help() {
         System.out.println(text);
     }
