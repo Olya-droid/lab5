cb9fe33e82fc16a78f2d738ae100fcfbabf07871
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:05:55 +0300

    появилось

--- src/Commands/Reorder.java
+++ src/Commands/Reorder.java
@@ -1,10 +1,18 @@
 package Commands;
 import java.util.Collections;
 
+/**
+ * Команда "ПЕРЕВЁРТЫШ"
+ */
 public class Reorder {
 
     static String description ="reorder: отсортировать коллекцию в порядке, обратном нынешнему.";
-public static void reorder (Routes.Collection c) {
+
+    /**
+     * Метод для разворота коллекции
+     * @param c коллекция
+     */
+    public static void reorder (Routes.Collection c) {
     Collections.reverse(c.Routes);
  }
 }
