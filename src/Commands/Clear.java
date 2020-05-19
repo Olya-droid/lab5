00d33fd4abc2036a126164965dd5dd747965a108
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 22:59:41 +0300

    Добавил описание

--- src/Commands/Clear.java
+++ src/Commands/Clear.java
@@ -1,7 +1,15 @@
 package Commands;
-import java.util.ArrayList;
+
+/**
+ * Команда "ЧИСТИЛЬЩИК "
+ */
 public class Clear {
     static String description ="clear : очистить коллекцию";
+
+    /**
+     * Метод для очистки коллекции, подаваемой на вход
+     * @param c коллекция
+     */
     public static void clear(Routes.Collection c) {
         c.Routes.clear();
     }
