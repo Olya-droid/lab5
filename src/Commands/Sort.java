e47ef6d145cf9466b9cd46101226401ef44c96a6
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:10:03 +0300

    Добавил описание

--- src/Commands/Sort.java
+++ src/Commands/Sort.java
@@ -2,9 +2,17 @@ package Commands;
 
 import java.util.Collections;
 
+/**
+ * Команда "СОРТИРОВКА"
+ */
 public class Sort {
         static String description ="sort: отсортиовать коллекцию в ествественном порядке.";
-        public static void  sort (Routes.Collection c) {
+
+    /**
+     * Метод для сортировки коллекции в ествественном порядке (по возрастанию id)
+     * @param c коллекция
+     */
+    public static void  sort (Routes.Collection c) {
            Collections.sort(c.Routes);
         }
 
