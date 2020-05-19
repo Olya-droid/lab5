b6136beb53bed7aa1b3b9f6f2730a145089f16eb
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:05:01 +0300

    Описание появилось

--- src/Commands/Print_field_descending_distance.java
+++ src/Commands/Print_field_descending_distance.java
@@ -5,20 +5,28 @@ import Routes.Route;
 
 import java.util.Arrays;
 
+/**
+ * Команда "Выведи-ка мне поле distance всех элементов коллекции, да в обратном порядке!"
+ */
 public class Print_field_descending_distance {
         static String description ="print_field_descending_distance: вывести значение поля distance в порядке убывания.";
-public static void print_field_descending_distance (Collection c) {
+
+        /**
+         * Метод для вывода поля distance всех элементов коллекции в обратном порядке
+         * @param c
+         */
+        public static void print_field_descending_distance (Collection c) {
         if (!c.Routes.isEmpty()) {
-                float[] array = new float[c.Routes.size()];
+                float[] array = new float[c.Routes.size()]; // создаётся массив размером с коллекцию
                 int i = 0;
                 for (Route r : c.Routes) {
-                        array[i] = r.getDistance();
+                        array[i] = r.getDistance(); // в массив вносятся значения distance
                         i++;
                 }
 
-                Arrays.sort(array);
+                Arrays.sort(array); // массив сортируется в порядке возрастания
                 for (i = c.Routes.size() - 1; i >= 0; i--) {
-                        System.out.println(array[i]);
+                        System.out.println(array[i]); // массив выводится в обратном порядке
                 }
         }else System.out.println("Коллекция пуста, в отличие от моего рабочего стола.");
 }
