ebcc8a9f59a1089171e14d62d1ed310e22020773
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:07:00 +0300

    Появилось

--- src/Commands/Show.java
+++ src/Commands/Show.java
@@ -2,8 +2,16 @@ package Commands;
 import Routes.Collection;
 import Routes.Route;
 
+/**
+ * Команда "ГЛЯДИ! "
+ */
 public class Show {
     static String description ="show : вывести в стандартный поток вывода все элементы коллекции в строковом представлении";
+
+    /**
+     * Метод для отображения всех элементов коллекции
+     * @param c коллекция
+     */
     public static void show (Collection c) {
         if (c.Routes.size() == 0) {
             System.out.println("Коллекция пуста, милорд");
