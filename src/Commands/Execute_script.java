426a7caa5eb376f4979b050e868cdf1027339b90
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:00:30 +0300

    добавил описание и ничего более :(

--- src/Commands/Execute_script.java
+++ src/Commands/Execute_script.java
@@ -1,8 +1,18 @@
 package Commands;
 
+/**
+ * Команда "СКРИПТ"
+ */
 public class Execute_script {
     static String description ="execute_script file_name : считать и исполнить скрипт из указанного файла.В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
-public static boolean execute_script(Routes.Collection c, String s2){
+
+    /**
+     * Метод для выполнения скрипта
+     * @param c коллекция
+     * @param s2 имя файла
+     * @return
+     */
+    public static boolean execute_script(Routes.Collection c, String s2){
 return true;
 }
 }
