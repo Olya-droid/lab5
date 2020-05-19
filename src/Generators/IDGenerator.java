6296d76913e93711d02a7e103ce933654f1dc490
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:27:10 +0300

    описание

--- src/Generators/IDGenerator.java
+++ src/Generators/IDGenerator.java
@@ -1,7 +1,13 @@
 package Generators;
 
+/**
+ * Генератор id 
+ */
 public class IDGenerator {
-
+    /**
+     * Метод для создания id
+     * @return id (Long)
+     */
     public static long generateNewID(){
         return (long) (1 + Math.random() * (Long.MAX_VALUE - 1));
     }
