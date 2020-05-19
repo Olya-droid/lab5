99b96af85c32596572bb272f70950ec6c55c1142
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-19 23:00:59 +0300

    добавил описание

--- src/Commands/Exit.java
+++ src/Commands/Exit.java
@@ -1,6 +1,15 @@
 package Commands;
+
+/**
+ * Команда "ВЫХОДА НЕТ"
+ */
 public class Exit {
     static String description ="exit : завершить программу (без сохранения в файл)";
+
+    /**
+     * Метод для прекращения работы программы
+     * @return false (передаётся в Main )
+     */
     public static boolean exit(){
         return false;
     }
