55abb3a6909ffb472f7a0215eadbd4c4fc451391
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-21 02:26:01 +0300

    Update Update.java

--- src/Commands/Update.java
+++ src/Commands/Update.java
@@ -1,5 +1,6 @@
 package Commands;
 import Readers.*;
+import Routes.Collection;
 import Routes.Coordinates;
 import Routes.Location;
 import Routes.Route;
@@ -11,7 +12,11 @@ import java.io.IOException;
  */
 public class Update {
         static String description ="update id {element} : обновить значение элемента коллекции, id которого равен заданному.";
-
+    static boolean changedName = false;  // показывает, было ли изменено имя элемента
+    static boolean changedCoordinates = false; // показывает, было ли изменено поле coordinates
+    static boolean changedFrom = false; //показывает, было ли изменено поле from
+    static boolean changedTo = false; // показывает, было ли изменено поле to
+    static boolean changedDist = false; // показывает, было ли изменено поле distance
     /**
      * Метод для изменения значений элемента по id
      * @param reader объект - считыватель
@@ -34,65 +39,204 @@ public class Update {
                int index =  c.Routes.indexOf(r);
 
                 System.out.print("\n" +"Давайте попробуем изменить элемент по введённому айди." + "\n" +
-                        "Раньше его звали \""+ c.Routes.get(index).getName() +"\". Как вы хотите, чтобы его звали теперь? ");
-                String name = reader.getLine();
-                if (name == null | name.equals("")) {
-                        System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                        return;
-                }
+                        "Раньше его звали \""+ c.Routes.get(index).getName() );
+            while (!changedName) changeName(reader, c, r);
+                System.out.print("\n" +"Раньше координаты были  "+  c.Routes.get(index).getCoordinates() );
+            while (!changedCoordinates) changeCoordinates(reader, c, r);
 
-
-
-                System.out.print("\n" +"Раньше координаты были  "+  c.Routes.get(index).getCoordinates() +"\n" + "Координаты теперь будут" + "\n" + "X = ");
-                int coordinateX = Checker.intChecker(reader.getLine());
-                if (coordinateX <= -836){
-                    System.out.println("Coordinate x должно быть больше -836");
-                    return;
-                }
-
-                System.out.print("Y = ");
-                float coordinateY = Checker.floatChecker(reader.getLine());
-                if (coordinateY >= 840){
-                    System.out.println("Coordinate y должно быть меньше 840");
-                    return;
-                }
-
-                System.out.print("\n" +"Раньше откуда было "+c.Routes.get(index).getFrom() +"\n" + "Откуда теперь? (from)" + "\n" + "x = ");
-                long locationFromX = Checker.longChecker(reader.getLine());
-                System.out.print("y = ");
-                double locationFromY = Checker.doubleChecker(reader.getLine());
-                System.out.print("Имя откуда:  ");
-                String locationFromName = reader.getLine();
-                if (locationFromName == null | locationFromName.equals("")) {
-                        System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                        return;
-                }
-
-                System.out.print("\n" +"Раньше куда было "+c.Routes.get(index).getTo() + "\n" +"Куда теперь? (to)" + "\n" + "x = ");
-                long locationToX = Checker.longChecker(reader.getLine());
-                System.out.print("y = ");
-                double locationToY = Checker.doubleChecker(reader.getLine());
-                System.out.print("Имя куда:  ");
-                String locationToName = reader.getLine();
-                if (locationToName == null | locationToName.equals("")) {
-                        System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                        return;
-                }
-                System.out.print("\n" +"Раньше Distance было "+c.Routes.get(index).getDistance() +"\n" + "Давай посмотрим, что у нас там с Distance теперь.  ");
-                float distance = Checker.floatChecker(reader.getLine());
-                if (!(distance > 1)) {
-                    System.out.println("Значение Distance должно быть больше 1");
-                    return;
-                }
-
-
-                c.Routes.get(index).setName(name);
-                c.Routes.get(index).setCoordinates(new Coordinates(coordinateX, coordinateY));
-                c.Routes.get(index).setFrom(new Location(locationFromX, locationFromY, locationFromName));
-                c.Routes.get(index).setTo(new Location(locationToX, locationToY, locationToName));
-                c.Routes.get(index).setDistance(distance);
-
+                System.out.print("\n" +"Раньше откуда было "+c.Routes.get(index).getFrom());
+            while (!changedFrom) changeFrom(reader, c, r);
+                System.out.print("\n" +"Раньше куда было "+c.Routes.get(index).getTo());
+            while (!changedTo) changeTo(reader, c, r);
+                System.out.print("\n" +"Раньше Distance было "+c.Routes.get(index).getDistance());
+            while (!changedDist) changeDist(reader, c, r);
                 System.out.println("\n" +"Вы достигли успеха в замене элемента по айди!");
+            changedName = false;
+            changedCoordinates = false;
+            changedFrom =false;
+            changedTo = false;
+            changedDist = false;
 
         }
+
+
+
+    /**
+     * Метод изменения имени в элемент
+     *
+     * @param reader объект - считыватель
+     * @param c      коллекция
+     * @param route  объект
+     * @throws IOException
+     */
+    protected static void changeName(Reader reader, Collection c, Route route) throws IOException {
+        System.out.print( "\n" + "Как вы хотите, чтобы его звали?  ");
+        String name = reader.getLine()+"";
+        if (name.equals("") ) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
+            return;
+        }
+        route.setName(name);
+        Update.changedName = true;
+    }
+
+    /**
+     * Метод изменения координат в элемент
+     *
+     * @param reader объект - считыватель
+     * @param c      коллекция
+     * @param route  объект
+     * @throws IOException
+     */
+    protected static void changeCoordinates(Reader reader, Collection c, Route route) throws IOException {
+        System.out.print("\n" + "Координаты, пожалуйста." + "\n" + "int X = ");
+        String s = reader.getLine()+"";
+        if (s.equals("") ) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        int coordinateX;
+        try {
+            coordinateX = Checker.intChecker(s);
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. Coordinate X должно быть типа int." + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (coordinateX <= -836) {
+            System.out.println("Coordinate x должно быть больше -836");
+            return;
+        }
+
+        System.out.print("float Y = ");
+        float coordinateY;
+        try {
+            coordinateY = Checker.floatChecker(reader.getLine());
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. Coordinate Y должно быть типа float" + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (coordinateY >= 840) {
+            System.out.println("Coordinate y должно быть меньше 840");
+            return;
+        }
+
+        route.setCoordinates(new Coordinates(coordinateX, coordinateY));
+        Update.changedCoordinates = true;
+    }
+
+    /**
+     * Метод изменения поля from в элемент
+     *
+     * @param reader объект - считыватель
+     * @param c      коллекция
+     * @param route  объект
+     * @throws IOException
+     */
+    protected static void changeFrom(Reader reader, Collection c, Route route) throws IOException {
+        System.out.print("\n" + "Откуда? (from)" + "\n" + "long x = ");
+
+        String s = reader.getLine()+"";
+        if (s.equals("") ) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        long locationFromX;
+        try {
+            locationFromX = Checker.longChecker(s);
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location From (X) должно быть типа long." + "\n Попробуем ещё разок!");
+            return;
+        }
+        System.out.print("double y = ");
+        double locationFromY;
+        try {
+            locationFromY = Checker.doubleChecker(reader.getLine());
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location From (Y) должно быть типа double." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("Имя откуда:  ");
+        String locationFromName = reader.getLine();
+        if (locationFromName.equals("") || locationFromName == null) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
+            return;
+        }
+        route.setFrom(new Location(locationFromX, locationFromY, locationFromName));
+        Update.changedFrom = true;
+    }
+
+
+    /**
+     * Метод изменения поля to в элемент
+     *
+     * @param reader объект - считыватель
+     * @param c      коллекция
+     * @param route  объект
+     * @throws IOException
+     */
+    protected static void changeTo(Reader reader, Collection c, Route route) throws IOException {
+        System.out.print("\n" + "Куда? (to)" + "\n" + "long x = ");
+        String s = reader.getLine()+"";
+        if (s.equals("") ) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        long locationToX;
+        try {
+            locationToX = Checker.longChecker(s);
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location To (X) должно быть типа long." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("double y = ");
+        double locationToY;
+        try {
+            locationToY = Checker.doubleChecker(reader.getLine());
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location To (Y) должно быть типа double." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("Имя куда:  ");
+        String locationToName = reader.getLine()+"";
+        if (locationToName.equals("") ) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
+            return;
+        }
+
+        route.setTo(new Location(locationToX, locationToY, locationToName));
+        Update.changedTo = true;
+    }
+
+    /**
+     * Метод изменения поля distance в элемент
+     *
+     * @param reader объект - считыватель
+     * @param c      коллекция
+     * @param route  объект
+     * @throws IOException
+     */
+    protected static void changeDist(Reader reader, Collection c, Route route) throws IOException {
+        System.out.print("\n" + "Давай посмотрим, что у нас там с Distance. (float)  ");
+        String s = reader.getLine()+"";
+        if (s.equals("")) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        float distance;
+        try {
+            distance = Checker.floatChecker(s);
+        } catch (NullPointerException e) {
+            System.out.println("Неправильный тип. distance должно быть типа float." + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (!(distance > 1)) {
+            System.out.println("Значение Distance должно быть больше 1");
+            return;
+        }
+        route.setDistance(distance);
+        Update.changedDist = true;
+    }
 }
