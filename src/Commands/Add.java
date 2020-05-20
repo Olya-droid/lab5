13afd440d6ba6aad9bcb8e185064722ca2db63fc
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-20 14:46:11 +0300

    Update Add.java

--- src/Commands/Add.java
+++ src/Commands/Add.java
@@ -6,11 +6,16 @@ import Routes.*;
 import java.io.IOException;
 
 /**
- * Команда "Добавить элемент"
+ * Команда "Добавить-ка ещё!"
  */
 
 public class Add {
     static String description = "add {element} : добавить новый элемент в коллекцию";
+    static boolean hasName = false;  // показывает, было ли добавлено имя элемента
+    static boolean  hasCoordinates = false; // показывает, было ли добавлено поле coordinates
+    static boolean  hasFrom = false; //показывает, было ли добавлено поле from
+    static boolean hasTo = false; // показывает, было ли добавлено поле to
+    static boolean hasDist = false; // показывает, было ли добавлено поле distance
 
     /**
      * Метод для добавления новых элементов в коллекцию
@@ -18,87 +23,183 @@ public class Add {
      * @param c коллекция
      * @throws NullPointerException
      */
-    public static void add(Reader reader, Collection c) throws NullPointerException {
-        try {
-            System.out.print("Давайте попробуем добавить элемент." + "\n" + "Как вы хотите, чтобы его звали?  ");
-            Route route = new Route();
-            route.setId(c.generateUniqueID());
+    public static void add(ConsoleSourceReader reader, Collection c) throws NullPointerException, IOException {
+        Route route = new Route();
+        route.setId(c.generateUniqueID());
+        while (!Add.hasName) addName(reader, c, route);
+        while (!Add.hasCoordinates) addCoordinates(reader, c, route);
+        while (!Add.hasFrom) addFrom(reader,c,route);
+        while (!Add.hasTo) addTo(reader, c, route);
+        while (!Add.hasDist) addDist(reader,c,route);
+        route.setCreationDate(java.time.LocalDate.now());
+        c.Routes.add(route);
+        System.out.println("Успех!");
+        hasName = false;
+        hasCoordinates = false;
+        hasFrom = false;
+        hasTo = false;
+        hasDist = false;
+        }
 
-            String name = reader.getLine();
-            if (name == null | name.equals("")) {
-                System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                return;
-            }
-            route.setName(name);
-
-            System.out.print("\n" + "Координаты, пожалуйста." + "\n" + "int X = ");
-            int coordinateX = Checker.intChecker(reader.getLine());
-            if (coordinateX <= -836){
-                System.out.println("Coordinate x должно быть больше -836");
-                return;
-            }
-
-            System.out.print("float Y = ");
-            float coordinateY = Checker.floatChecker(reader.getLine());
-            if (coordinateY >= 840){
-                System.out.println("Coordinate y должно быть меньше 840");
-                return;
-            }
-
-            route.setCoordinates(new Coordinates(coordinateX, coordinateY));
-
-
-            System.out.print("\n" + "Откуда? (from)" + "\n" + "long x = ");
-            long locationFromX = Checker.longChecker(reader.getLine());
-
-            System.out.print("double y = ");
-            double locationFromY = Checker.doubleChecker(reader.getLine());
-
-            System.out.print("Имя откуда:  ");
-            String locationFromName = reader.getLine();
-            if (locationFromName == null | locationFromName.equals("")) {
-                System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                return;
-            }
-            route.setFrom(new Location(locationFromX, locationFromY, locationFromName));
-
-            System.out.print( "\n" +"Куда? (to)" + "\n" + "long x = ");
-            long locationToX = Checker.longChecker(reader.getLine());
-
-            System.out.print("double y = ");
-            double locationToY = Checker.doubleChecker(reader.getLine());
-
-            System.out.print("Имя куда:  ");
-            String locationToName = reader.getLine();
-            if (locationToName == null | locationToName.equals("")) {
-                System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
-                return;
-            }
-
-            route.setTo(new Location(locationToX, locationToY, locationToName));
-
-
-            System.out.print("\n" + "Давай посмотрим, что у нас там с Distance. (float)  ");
-            float distance = Checker.floatChecker(reader.getLine());
-            if (!(distance > 1)) {
-                System.out.println("Значение Distance должно быть больше 1");
-                return;
-            }
-            route.setDistance(distance);
-
-
-            route.setCreationDate(java.time.LocalDate.now());
-
-
-            c.Routes.add(route);
-            System.out.println("Успех!");
-
-        } catch (NullPointerException e) {
+    /**
+     * Метод добавления имени в элемент
+     * @param reader объект - считыватель
+     * @param c коллекция
+     * @param route объект
+     */
+    protected static void addName (ConsoleSourceReader reader, Collection c, Route route){
+        System.out.print("Давайте попробуем добавить элемент." + "\n" + "Как вы хотите, чтобы его звали?  ");
+        String name = reader.getLine();
+        if (  name.equals("") || name == null) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
             return;
-        } catch (IOException e) {
-            System.out.println("Ту появилась ошибочка значит вы сделали что-то не так");
+        }
+        route.setName(name);
+        Add.hasName = true;
+    }
+
+    /**
+     * Метод добавления координат в элемент
+     * @param reader объект - считыватель
+     * @param c коллекция
+     * @param route объект
+     */
+    protected static void addCoordinates (ConsoleSourceReader reader, Collection c, Route route){
+        System.out.print("\n" + "Координаты, пожалуйста." + "\n" + "int X = ");
+        String s = reader.getLine();
+        if (  s.equals("") || s == null) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        int coordinateX;
+        try { coordinateX = Checker.intChecker(s);}
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. Coordinate X должно быть типа int." + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (coordinateX <= -836){
+            System.out.println("Coordinate x должно быть больше -836");
             return;
         }
 
+        System.out.print("float Y = ");
+        float coordinateY;
+        try {coordinateY = Checker.floatChecker(reader.getLine());}
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. Coordinate Y должно быть типа float" + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (coordinateY >= 840){
+            System.out.println("Coordinate y должно быть меньше 840");
+            return;
+        }
+
+        route.setCoordinates(new Coordinates(coordinateX, coordinateY));
+        Add.hasCoordinates = true;
+    }
+
+    /**
+     * Метод добавления поля from в элемент
+     * @param reader объект - считыватель
+     * @param c коллекция
+     * @param route объект
+     */
+    protected static void addFrom(ConsoleSourceReader reader, Collection c, Route route){
+        System.out.print("\n" + "Откуда? (from)" + "\n" + "long x = ");
+
+        String s = reader.getLine();
+        if (  s.equals("") || s == null) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        long locationFromX;
+       try{ locationFromX = Checker.longChecker(s);}
+       catch (NullPointerException e) {
+           System.out.println("Неправильный тип. location From (X) должно быть типа long." + "\n Попробуем ещё разок!" );
+           return;
+       }
+        System.out.print("double y = ");
+        double locationFromY;
+        try {locationFromY = Checker.doubleChecker(reader.getLine());}
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location From (Y) должно быть типа double." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("Имя откуда:  ");
+        String locationFromName = reader.getLine();
+        if (locationFromName.equals("") || locationFromName == null) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
+            return;
+        }
+        route.setFrom(new Location(locationFromX, locationFromY, locationFromName));
+        Add.hasFrom = true;
+    }
+
+
+    /**
+     * Метод добавления поля to в элемент
+     * @param reader объект - считыватель
+     * @param c коллекция
+     * @param route объект
+     */
+    protected static void  addTo (ConsoleSourceReader reader, Collection c, Route route){
+        System.out.print( "\n" +"Куда? (to)" + "\n" + "long x = ");
+        String s = reader.getLine();
+        if (  s.equals("") || s == null) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        long locationToX;
+        try { locationToX = Checker.longChecker(s);}
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location To (X) должно быть типа long." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("double y = ");
+        double locationToY;
+        try {locationToY = Checker.doubleChecker(reader.getLine());}
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. location To (Y) должно быть типа double." + "\n Попробуем ещё разок!");
+            return;
+        }
+
+        System.out.print("Имя куда:  ");
+        String locationToName = reader.getLine();
+        if ( locationToName.equals("") || locationToName == null) {
+            System.out.println("Пустая строка ни к чему не приведёт. Пока ты смотришь в пустую строку, пустая строка смотрит в тебя...");
+            return;
+        }
+
+        route.setTo(new Location(locationToX, locationToY, locationToName));
+        Add.hasTo = true;
+    }
+
+    /**
+     * Метод добавления поля distance в элемент
+     * @param reader объект - считыватель
+     * @param c коллекция
+     * @param route объект
+     */
+    protected static void addDist(ConsoleSourceReader reader, Collection c, Route route){
+        System.out.print("\n" + "Давай посмотрим, что у нас там с Distance. (float)  ");
+        String s = reader.getLine();
+        if (  s.equals("") || s == null) {
+            System.out.println("Введена пустая строка. Не надо так.");
+            return;
+        }
+        float distance;
+        try { distance = Checker.floatChecker(s); }
+        catch (NullPointerException e) {
+            System.out.println("Неправильный тип. distance должно быть типа float." + "\n Попробуем ещё разок!");
+            return;
+        }
+        if (!(distance > 1)) {
+            System.out.println("Значение Distance должно быть больше 1");
+            return;
+        }
+        route.setDistance(distance);
+        Add.hasDist = true;
     }
 }
