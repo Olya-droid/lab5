25aedc8a27b451be89e471cf89493d0de72351d5
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-21 01:51:00 +0300

    Update Execute_script.java

--- src/Commands/Execute_script.java
+++ src/Commands/Execute_script.java
@@ -37,36 +37,43 @@ public class Execute_script {
             System.out.println("спасибо, но в следующий раз введите его в той же строке, что и команду" + "\n");
         }
         if (theSameExist(s2)){
-            System.out.println("ать с рекурсией не надо игр");
+            System.out.println("\n"+"-ать с рекурсией не надо игр-" +"\n");
+            System.out.println("Программа была завершена во избежание разрыва пространственно-временного континуума."+ "\n" +  "\n" + "\n" +"А ещё, кстати:");
             fileWork = false;
-        }
-        usedFiles.add(s2);
-        try {
-            FileSourceReader fileSourceReader = new FileSourceReader(s2);
-            String[] s;
-            s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
-            while (fileWork && s != null) {
-                fileWork = Commander.switcher(fileSourceReader, c, s[0], s[1]);
+            return fileWork;
+        } else {
+            usedFiles.add(s2);
+            try {
+                FileSourceReader fileSourceReader = new FileSourceReader(s2);
+                String[] s;
                 s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
-            }
-            fileSourceReader.close();
-            usedFiles.clear();
-            System.out.println("файл успешно дочитался");
-        } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-            usedFiles.clear();
-        } catch (IOException e) {
-            System.out.println("неправильный формат ввода расположения файла");
-            usedFiles.clear();
-        } catch (Exception e) {
-            System.out.println("непредвиденный конец файла");
-            usedFiles.clear();
-        }
+                while (fileWork && s != null) {
+                    fileWork = Commander.switcher(fileSourceReader, c, s[0], s[1]);
+                    s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
 
+                }
+                fileSourceReader.close();
+                usedFiles.clear();
+                System.out.println("Завершение скрипта");
+            } catch (FileNotFoundException e) {
+                System.out.println(e.getMessage());
+                usedFiles.clear();
+            } catch (IOException e) {
+                System.out.println("неправильный формат ввода расположения файла");
+                usedFiles.clear();
+            } catch (Exception e) {
+                System.out.println("непредвиденный конец файла");
+                usedFiles.clear();
+            }
+        }
         return fileWork;
     }
 
-
+    /**
+     * Поиск передаваемого файла среди уже запущенных
+     * @param s2 путь файла
+     * @return true - если найден
+     */
     private static boolean theSameExist (String s2){
         for (String s: usedFiles) if (s.equals(s2)) return true;
         return false;
