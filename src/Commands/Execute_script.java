a76946a5f83fe4d28d80c7510c49dc5a4e656e00
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-20 15:53:47 +0500

    Update Execute_script.java

--- src/Commands/Execute_script.java
+++ src/Commands/Execute_script.java
@@ -13,6 +13,34 @@ public class Execute_script {
      * @return
      */
     public static boolean execute_script(Routes.Collection c, String s2){
-return true;
-}
+        if (s2==null | s2.equals("")) {
+            System.out.println("кажется, вы забыли ввести расположение файла. Где он лежит?");
+            ConsoleSourceReader consoleSourceReader = new ConsoleSourceReader();
+            s2 = consoleSourceReader.getLine();
+            System.out.println("спасибо, но в следующий раз введите его в той же строке, что и команду");
+        }
+
+        try{
+            FileSourceReader fileSourceReader = new FileSourceReader(s2);
+            boolean fileWork = true;
+            String [] s;
+            while (fileWork) {
+                s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
+                fileWork = Commander.switcher(fileSourceReader, c, s[0], s[1]);
+                while (s != null) {
+                    System.out.println(s[0] + " " + s[1]);
+                    s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
+                }
+            }
+            System.out.println("файл успешно дочитался");
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println("ошибочка мб чета введено не то");
+        } catch (Exception e) {
+            System.out.println("походу непредвиденный конец");
+        }
+        
+        return true;
+    }
 }
