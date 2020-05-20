5e114d47ab9e66ec5a22168d7431b8e979ce3e96
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-20 21:00:08 +0500

    Update Execute_script.java

--- src/Commands/Execute_script.java
+++ src/Commands/Execute_script.java
@@ -4,49 +4,55 @@ import Exceptions.FileNotFoundException;
 import Readers.CommandArgumentSplitter;
 import Readers.ConsoleSourceReader;
 import Readers.FileSourceReader;
+
 import java.io.IOException;
 
 /**
  * Команда "СКРИПТ"
  */
+
 public class Execute_script {
-    static String description ="execute_script file_name : считать и исполнить скрипт из указанного файла.В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
+
+    static String description = "execute_script file_name : считать и исполнить скрипт из указанного файла.В скрипте содержатся команды в таком же виде, в котором их вводит пользователь в интерактивном режиме.";
 
     /**
      * Метод для выполнения скрипта
-     * @param c коллекция
+     *
+     * @param c  коллекция
      * @param s2 имя файла
-     * @return
+     * @return fileWork 
      */
-    public static boolean execute_script(Routes.Collection c, String s2){
-        if (s2==null | s2.equals("")) {
+
+    public static boolean execute_script(Routes.Collection c, String s2) {
+
+        boolean fileWork = true;
+
+        if (s2 == null | s2.equals("")) {
             System.out.println("кажется, вы забыли ввести расположение файла. Где он лежит?");
             ConsoleSourceReader consoleSourceReader = new ConsoleSourceReader();
             s2 = consoleSourceReader.getLine();
-            System.out.println("спасибо, но в следующий раз введите его в той же строке, что и команду");
+            System.out.println("спасибо, но в следующий раз введите его в той же строке, что и команду" + "\n");
         }
 
-        try{
+        try {
             FileSourceReader fileSourceReader = new FileSourceReader(s2);
-            boolean fileWork = true;
-            String [] s;
-            while (fileWork) {
-                s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
+            String[] s;
+            s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
+            while (fileWork && s != null) {
                 fileWork = Commander.switcher(fileSourceReader, c, s[0], s[1]);
-                while (s != null) {
-                    System.out.println(s[0] + " " + s[1]);
-                    s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
-                }
+                s = CommandArgumentSplitter.comArgSplitter(fileSourceReader.getLine());
             }
+            fileSourceReader.close();
             System.out.println("файл успешно дочитался");
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } catch (IOException e) {
-            System.out.println("ошибочка мб чета введено не то");
+            System.out.println("неправильный формат ввода расположения файла");
         } catch (Exception e) {
-            System.out.println("походу непредвиденный конец");
+            System.out.println("непредвиденный конец файла");
         }
-        
-        return true;
+
+        return fileWork;
     }
+
 }
