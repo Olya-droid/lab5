bd830cba0e669b93536bf3726fb3a8898915d816
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-20 15:04:54 +0300

    import java.io.File;

--- src/Readers/FileSourceReader.java
+++ src/Readers/FileSourceReader.java
@@ -6,7 +6,7 @@ import Exceptions.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.File;
 /**
  * Класс для чтения из файла 
  */
