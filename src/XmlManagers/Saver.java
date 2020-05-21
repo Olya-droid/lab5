b22cf7898370e8e6868f4633159bde463f08491e
оля <61423764+Olya-droid@users.noreply.github.com>
2020-05-21 06:04:22 +0500

    Update Saver.java

--- src/XmlManagers/Saver.java
+++ src/XmlManagers/Saver.java
@@ -20,7 +20,7 @@ public class Saver {
      * @param path путь до файла
      * @throws java.io.FileNotFoundException
      */
-    public static void saveCollection(Collection c, String path) throws java.io.FileNotFoundException {
+    public static void saveCollection(Collection c, String path) throws java.io.FileNotFoundException{
 
         try{
             File newCollection = new File(path);
@@ -30,6 +30,7 @@ public class Saver {
             PrintWriter printWriter = new PrintWriter(newCollection);
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             marshaller.marshal(c, printWriter);
+            printWriter.close();
         }catch (JAXBException e){
             System.out.println("Некорректный файл");
         }catch (FileNotFoundException e){
