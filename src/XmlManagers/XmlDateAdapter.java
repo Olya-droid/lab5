4cc8571a44a3cae313496217bc18c078edbf2ef4
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-27 22:12:53 +0300

    final

--- src/XmlManagers/XmlDateAdapter.java
+++ src/XmlManagers/XmlDateAdapter.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 /**
- * Класс для адаптации даты к xml 
+ * Класс для адаптации даты к xml
  */
 
 public class XmlDateAdapter extends XmlAdapter<String, LocalDate> {
@@ -12,9 +12,7 @@ public class XmlDateAdapter extends XmlAdapter<String, LocalDate> {
     public LocalDate unmarshal(String v) throws Exception {
         return LocalDate.parse(v);
     }
-
     public String marshal(LocalDate v) throws Exception {
         return v.toString();
     }
 }
-
