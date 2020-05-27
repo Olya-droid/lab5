f5b155b80924594ace9c25838db8aa6bb3b583ba
P1oruN7 <61423758+P1oruN7@users.noreply.github.com>
2020-05-27 22:04:54 +0300

    final

--- src/Readers/CommandArgumentSplitter.java
+++ src/Readers/CommandArgumentSplitter.java
@@ -5,28 +5,21 @@ package Readers;
  */
 public class CommandArgumentSplitter {
     /**
-     * Метод для разделения вводимой строки на две и удаления пробелов 
+     * Метод для разделения вводимой строки на две и удаления пробелов
      * @param readLine строка
-     * @return массив строк из двух элементов или null 
+     * @return массив строк из двух элементов или null
      */
     public static String[] comArgSplitter(String readLine) {
-
-        if (readLine != null) {
-
+        if (readLine != null && !readLine.equals("")) {
             readLine = readLine.trim();
             String [] ComArg = readLine.split(" ", 2);
-
             if (ComArg.length == 2) {
                 ComArg[1] = ComArg[1].trim();
                 return ComArg;
             } else
                 return new String[]{ComArg[0], ""};
-
         }
-
-        System.out.println("Вы не ввели команду. Попробуйте еще раз");
+        System.out.println("\n"+"Вы не ввели команду. Попробуйте еще раз");
         return null;
-
     }
-
 }
